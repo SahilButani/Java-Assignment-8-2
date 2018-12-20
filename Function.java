fec94bbb551d7a4702ae5595afa75727cf8779f1
SahilButani <sahilbutani1760@gmail.com>
2018-12-20 15:40:05 +0530

    Add files via upload

---
+++ Function.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Function {
+    static boolean func(String str){
+        boolean[] b=new boolean[26];
+        for(int i=0;i<str.length();i++){
+            if(str.charAt(i)>=97&&str.charAt(i)<=122)
+                b[str.charAt(i)-'a']=true;
+        }
+        for(int i=0;i<26;i++)
+            if(b[i]==false)
+                return false;
+            return true;
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner();
+        String str=sc.next();
+        if(func(str))
+        System.out.println("String contains all the characters of alphabet");
+        else
+            System.out.println("String does not contain all the characters of alphabet");
+    }
+}
