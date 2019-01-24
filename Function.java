e4677c45de3957fdcb6e103db147016a73fc9c63
Sahil Butani <sahilbutani1760@gmail.com>
2019-01-24 15:21:30 +0530

    Update Function.java

--- Function.java
+++ Function.java
@@ -1,21 +1,30 @@
 import java.util.Scanner;
 
 public class Function {
-    static boolean func(String str){
-        boolean[] b=new boolean[26];
-        for(int i=0;i<str.length();i++){  
-            if(str.charAt(i)>=97&&str.charAt(i)<=122)
-                b[str.charAt(i)-'a']=true;
+    /*
+     * This function takes string as input and checks if all the characters
+     * from 'a' to 'z' are present or not. If all characters are present 
+     * return true.
+     */
+    static boolean func(String input_string){
+        boolean[] isCharacter=new boolean[26];
+        for(int i=0;i<input_string.length();i++){  
+            if(input_string.charAt(i)>=97&&input_string.charAt(i)<=122)
+                isCharacter[input_string.charAt(i)-'a']=true;
         }
         for(int i=0;i<26;i++)
-            if(b[i]==false)
+            if(isCharacter[i]==false)
                 return false;
             return true;
     }
+    /*
+     * Main method takes input string from user and passes it to checkAllAlphabets function
+     * If function returns boolean value and prints the output accordingly.
+     */
     public static void main(String args[]){
         Scanner sc=new Scanner(System.in);
-        String str=sc.nextLine();
-        if(func(str))
+        String input_string=sc.nextLine();
+        if(func(input_string))
         System.out.println("String contains all the characters of alphabet");
         else
             System.out.println("String does not contain all the characters of alphabet");
